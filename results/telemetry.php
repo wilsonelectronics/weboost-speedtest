ed8783d9f2925a516b8de720d37d5ffb7d988e40
Dylan Sharp <dgstatic@gmail.com>
2026-03-25 14:16:27 -0600

    url parameter, privacy update

--- results/telemetry.php
+++ results/telemetry.php
@@ -4,7 +4,7 @@ require 'telemetry_settings.php';
 require_once 'telemetry_db.php';
 require_once '../backend/getIP_util.php';
 
-$userId = $_COOKIE['userId'];
+$userId = $_GET['user'];
 $ip = getClientIp();
 $ispinfo = $_POST['ispinfo'];
 $extra = $_POST['extra'];
